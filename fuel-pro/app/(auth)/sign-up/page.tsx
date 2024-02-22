e55b52dec6692a71c7bd05eeb59d5d3d31b2019a
Ayush Patel <patelayush1920@gmail.com>
2024-02-21 23:07:03 -0600

    update dashboard

--- fuel-pro/app/(auth)/sign-up/page.tsx
+++ fuel-pro/app/(auth)/sign-up/page.tsx
@@ -10,11 +10,11 @@ import GithubSignInButton from '@/app/components/GithubSignInButton'
 import GoogleSignInButton from '@/app/components/GoogleSignInButton'
 
 async function SignUp() {
-    const session = await getServerSession(authOptions);
+    // const session = await getServerSession(authOptions);
 
-    if(session){
-        return redirect('/dashboard');
-    }
+    // if(session){
+    //     return redirect('/dashboard');
+    // }
 
     return (
         <div className=' mt-24 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-sm md:px-14 '>
@@ -22,12 +22,13 @@ async function SignUp() {
                 <h1 className=' text-3xl font-semibold text-white'>Sign up</h1>
                 <div className=' space-y-4 mt-5'>
                     <Input type='email' name='email' placeholder='Email' className=' bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block' />
+                    <Input type='password' name='password' placeholder='password' className=' bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block' />
                     <Button type='submit' variant='destructive' className=' w-full text-black bg-[#bbb9b9] hover:bg-slate-500'>Sign up</Button>
                 </div>
             </form>
             <div className='text-gray-400 text-sm mt-2'>
-                ALready have an account?
-                <Link className=' text-white' href='/login'> Log in now!! </Link>
+                Already have an account?
+                <Link className=' text-white ml-2' href='/login'>  Log in now!! </Link>
             </div>
 
             <div className=' flex w-full justify-center items-center gap-x-3 mt-6'>
--- fuel-pro/app/dashboard/page.tsx
+++ fuel-pro/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import dynamic from 'next/dynamic';
 import NavBar from '../components/NavBar';
+import Link from 'next/link';
 
 
 function Dashboard() {
@@ -32,14 +33,59 @@ function Dashboard() {
     const dynamicHeight = (9 / 16) * dynamicWidth; // Assuming a 16:9 aspect ratio
 
     return (
-        
-        <div>
+
+        <div className='relative'>
             <NavBar />
-            <Image className=' h-screen flex items-center justify-center bg-cover bg-center -z-50 ' src='/dashboard_image.avif' alt={''} width={dynamicWidth}
-            <NavBar />
-            <Image className=' h-screen flex items-center justify-center bg-cover bg-center -z-50 md:bg-transparent brightness-50 ' src='/dashboard_image.avif' alt={''} width={dynamicWidth}
-                height={dynamicHeight} layout={{type: "responsive"}} ></Image>
-            <div className=' absolute left-1 top-1 z-10 text-3xl'></div>
+
+            {/* Background Image */}
+            <Link href='/dashboard'>
+            <Image
+                className='w-full h-screen object-cover object-center brightness-50 md:bg-transparent'
+                src='/dashboard_image.avif'
+                alt={''}
+                
+                width={dynamicWidth}
+                height={dynamicHeight}
+                layout='responsive'
+            />
+            </Link>
+
+            <div className=' border bg-blue/70 rounded-lg '>
+                <div className=' border bg-blue/70 rounded-lg px-[10%] pt-[2%] pb-[5%] bg-blue-400 bg-opacity-40 absolute mt-[7%] top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-red-600'>
+
+
+                    <center>
+                        <h1 className='text-4xl font-bold'>Get your fuel quote whenever and wherever!!!</h1>
+                    </center>
+                    <br />
+                    <div className='  border rounded-2xl bg-yellow-300/30 px-14 pt-7 pb-28 '>
+                        <center>
+                            <h1 className=' text-green-800 text-4xl font-bold font-serif'>Save Money and Time!!</h1>
+                        
+                        <br />
+                        <br />
+                        <h1 className=' text-black italic font-semibold'>
+                        Don't want to waste time at gas station? Do not worry !! Fuel Go has you covered !!
+                        <br />
+                        
+                        
+                        We do it cheap, easy and fast. Why bother with long queue at gas
+              stations, when the fuel can come to you?
+                        </h1>
+                        <br />
+                        <a href='/fuel-quote' className=' text-black underline p-2 italic bg-white rounded-md hover:bg-gray-300 hover:text-red-400 '>
+                            Get you free quote today!!
+                            
+                            
+                        </a>
+                        </center>
+                        <br />
+                        <center>
+                        <h3 className=' text-black'>Follow us on:</h3>
+                        </center>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
